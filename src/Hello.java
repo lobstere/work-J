86bec6cc7c0013c347f4258f9eb29ac5c1a30976
henry <lobster.love39@gmail.com>
2022-11-03 09:32:05 +0800

    初始化

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,23 @@
+import java.util.*;
+
 public class Hello {
-    public int[] runningsum(int[] nums) {
+    static Scanner sc = new Scanner(System.in);
+    public static int[] runningsum(int[] nums) {
         for(int i=1;i<nums.length;i++){
             nums[i]+=nums[i-1];
         }
         return nums;
+    }
+
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数组，并以回车结束：");
+        for(int i = 0; i < a.length; i++){
+            a[i] = sc.nextInt();
+        }
+        //直接打印数组a出来的是数组的首地址，必须用toString方法
+        System.out.println("数组a为:" + Arrays.toString(a));
+        runningsum(a);
     }
 }
