47e1bc4f210f52ba55f994a4734177668592b77b
henry <lobster.love39@gmail.com>
2022-11-03 09:58:20 +0800

    初始化

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Hello {
     static Scanner sc = new Scanner(System.in);
-    public static int[] runningsum(int[] nums,int c) {
-        for(int i=1;i<c;i++){
+    public static int[] runningsum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
             nums[i]+=nums[i-1];
         }
         return nums;
@@ -12,12 +12,12 @@ public class Hello {
     public static void main(String[] args) {
         int[] a = new int[4];
         System.out.println("请输入数组，并以回车结束：");
-        String b = sc.nextLine();
-        int c = Integer.parseInt(String.valueOf(b.split(" ").length));
-        for(int i = 0; i < c; i++){
+//        String b = sc.nextLine();
+//        int c = Integer.parseInt(String.valueOf(b.split(" ").length));
+        for(int i = 0; i < a.length; i++){
             a[i] = sc.nextInt();
         }
-        System.out.println(Arrays.toString(runningsum(a,c)));
-        System.out.println("c:"+c);
+        System.out.println(Arrays.toString(runningsum(a)));
+//        System.out.println("c:"+c);
     }
 }
