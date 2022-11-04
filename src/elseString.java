f099414eb9e939c161f270a6bd3ef3418cf59ef0
henry <lobster.love39@gmail.com>
2022-11-04 18:54:37 +0800

    初始化

---
+++ src/elseString.java
@@ -0,0 +1,26 @@
+public class elseString {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        //记录杂志字符串出现的次数
+        int[] arr = new int[26];
+        int temp;
+        for (int i = 0; i < magazine.length(); i++) {
+            temp = magazine.charAt(i) - 'a';
+            arr[temp]++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            temp = ransomNote.charAt(i) - 'a';
+            //对于金信中的每一个字符都在数组中查找
+            //找到相应位减一，否则找不到返回false
+            if (arr[temp] > 0) {
+                arr[temp]--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canConstruct("a","b"));
+    }
+}
