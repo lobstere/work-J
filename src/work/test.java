893b59d9a3aaa03a15410709d9fb383683668144
henry <lobster.love39@gmail.com>
2022-11-14 18:32:38 +0800

    初始化

--- src/work/test.java
+++ src/work/test.java
@@ -5,5 +5,11 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //  获取数组的方式
+        String[] n = sc.nextLine().split(",");
+        int[] nums = new int[n.length];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=Integer.parseInt(n[i]);
+        }
     }
 }
